8517cf2bda75fc0cf8388a580d19093ded5469b8
Anderson Lomba <andersonlomba172@outlook.com>
2023-03-03 12:20:01 -0300

    Primeiro commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,69 @@
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+import Header from '../../components/Header'
+import Balance from '../../components/Balance';
+import Movements from '../../components/Movements';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Header  name = "Anderson Lomba"/>
+      
+      <Balance saldo ="7.770.00" gastos="-230.40"/>
+
+      <Text style={styles.title} >Últimas movimentações</Text>
+
+      <FlatList
+       
+       style={styles.list}
+       data= {list}
+       keyExtractor={(item) => String(item.id)}
+       showsVerticalScrollIndicator={false}
+      renderItem={({item}) => <Movements data={item}/>}
+      />
+
+    </View>
+  );
+}
+
+const list = [
+  {
+    id: 1,
+    label: 'Boleto da Internet',
+    value: '79,90',
+    date: '17/07/2023',
+    type: 0 // despesas
+  },
+  {
+    id: 2,
+    label: 'Pagamento',
+    value: '7007,50',
+    date: '22/09/2023',
+    type: 1 // receita
+  },
+  {
+    id: 3,
+    label: 'Pix de Cliente A',
+    value: '709,90',
+    date: '07/07/2023',
+    type: 1 // receita
+  }
+]
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fbfbfb',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 14,
+    marginRight: 14,
+    marginTop: 14,
+  },
+  list:{
+    marginStart: 14,
+    marginEnd: 14,
+  }
+});
